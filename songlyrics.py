6b4bcf3033b08af310c7b4221669b74c7d6f83e4
menezesmario <mario082@gmail.com>
2021-01-22 10:34:46 -0300

    songlyrics ok

--- songlyrics.py
+++ songlyrics.py
@@ -1,3 +1,5 @@
+print('Anamauê, auêia, aê', '\n','Anamauê, auêia, aê', '\n','Anamauê, auêia, aê', '\n', 'Anamauê') 
 lyrics = input("Paste here the lyrics of the song you like: ")
 
 print(lyrics)
+
