f7885c0061206f78cde24d5c3063e1d5626fda34
menezesmario <mario082@gmail.com>
2021-01-22 10:20:35 -0300

    label

--- label.py
+++ label.py
@@ -1,2 +1,11 @@
-Nome = input("Enter your full name: ")
+name = input("Enter your full name: ")
+adress = input("Enter your adress: ")
+cep = input("Enter your Zip code: ")
+phone = input("Enter your phone number: ")
+
+print(name)
+print(adress)
+print(cep, ', ', phone)
+
+
 
