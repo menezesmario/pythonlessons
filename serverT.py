e73efd4e8d7d133246a9db70a2dac04c8fa07240
menezesmario <mario082@gmail.com>
2021-01-26 17:34:33 -0300

    Server config ok

--- serverT.py
+++ serverT.py
@@ -9,36 +9,37 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        logging.info("GET request, \nPath: %s\nHeaders: \n%s\n", str(self.path), str(self.headers))
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("Servidor ok".format(self.path).encode('utf-8'))
 
     def do_POST(self):
-        content_length = int(self.headers["Content-Length"])
-        post_data = self.rfile.read(content_length)
-        logging.info("POST request, \nPath: %s\n%s\nBody: \n%s\n", str(self.path), str(self.headers),
-                     post_data.decode('utf-8'))
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
-        self_wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-    def run(server_class=HTTPServer, handler_class=S, port=8080):
-        logging.basicConfig(level=logging.INFO)
-        server_adress = ('', port)
-        httpd = server_class(server_adress, handler_class)
-        logging.info('Starting httpd...  \n')
-        try:
-            httpd.server_forever()
-        except KeyboardInterrupt:
-            pass
-        httpd.server_close()
-        logging.info('Stopping httpd...\n')
 
-        if __name__ == '__main__':
-            from sys import argv
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
 
-            if len(argv) == 2:
-                run(port=int(argv[1]))
 
-            else:
-                run()
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
