90b9fcb5e9f54ee65015d92ae449c5272d701904
menezesmario <mario082@gmail.com>
2021-01-26 15:44:04 -0300

    Discount calculation

---
+++ discount.py
@@ -0,0 +1,13 @@
+#Valor do produto
+price=float(input("Price: "))
+
+#Discount 12%
+discounted_price=price * 0.12
+
+#New value
+new_value=price * 0.88
+
+#Print on Screen
+print("Price: R$", price)
+print("Discount ", discounted_price)
+print("Value with discount: ", new_value)
